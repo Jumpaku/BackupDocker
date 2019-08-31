3624e8f2377e970fd52dcd360a6f5d35cef76696
Jumpaku <itzimeru@gmail.com>
2019-08-31 21:10:28 +0900

    add files

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z $NC_USER ]; then
+    echo "environment NC_USER is required."
+    exit 2
+fi
+
+if [ -z $NC_PASSWORD ]; then
+    echo "environment NC_PASSWORD is required."
+    exit 2
+fi
+
+if [ -z $NC_URL ]; then
+    echo "environment NC_URL is required."
+    exit 2
+fi
+
+
+echo "$CRON_EXP $CRON_USER /backup.sh && nextcloudcmd --non-interactive --user $NC_USER --password $NC_PASSWORD --exclude /sync-exclude.lst /backup/ $NC_URL" >> /etc/crontab
+
+cron -f
